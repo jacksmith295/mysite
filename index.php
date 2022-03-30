24c41bbebbe3d3fee6d5f434df81942718441f31
Jack <c0037268@my.shu.ac.uk>
2022-03-30 18:02:08 +0100

    Added login button and tim diss

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <link rel = "stylesheet" href = "assets/css/stylesheet.css">
     </head>
     
-    <h1>Hello World!</h1>
+    <h1>Tim Smells</h1>
     
+    <input type="button" value="Login" id="login">
 </html>
